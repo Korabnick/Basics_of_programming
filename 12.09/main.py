c5f7f1f559c0b48145f9b032b786e7473b76f20e
Korabnick <dragonytnik@gmail.com>
2022-09-13 20:31:52 +0200

    Добавлена домашка за 12.09

---
+++ 12.09/main.py
@@ -0,0 +1,26 @@
+lower_s = 0
+lower_c = 0
+super_s = 0
+super_c = 0
+strings = 'som eBO DYo nce TOL Dme'
+print('Strings: ' + strings)
+strings = strings.split()
+string_len = len(strings)
+for substr in strings:
+    super_c = 0
+    lower_c = 0
+    for uchar in substr:
+        if uchar.isupper():
+            super_c += 1
+        else:
+            lower_c += 1
+    if super_c > lower_c:
+        super_s += 1
+    else:
+        lower_s += 1
+    print (super_c,lower_c)
+
+one = super_s/string_len * 100
+two = lower_s/string_len * 100
+
+print("Заглавных букв: {0}%, Строчных букв: {1}%".format(int(one),int(two)))
