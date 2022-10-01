09cad6e8c88e43c552df0dadb7988ccc765e55d9
Korabnick <dragonytnik@gmail.com>
2022-10-01 11:36:50 +0200

    Домашнее задание за 19.09

---
+++ 1909/function.py
@@ -0,0 +1,23 @@
+def date_checker(d, m, y):
+    months_31 = (1, 3, 5, 7, 8, 10, 12)
+    months_30 = (4, 6, 9, 11)
+    if m == months_31:
+        if 1 <= d <= 31:
+            return True
+        else:
+            return False
+    elif m == months_30:
+        if 1 <= d <= 30:
+            return True
+        else:
+            return False
+    elif y % 4 == 0 and y % 100 != 0 or y % 400 == 0:
+        if m == 2 and 1 <= d <= 29:
+            return True
+        else:
+            return False
+    elif y % 4 != 0 or y % 400 !=0:
+        if m == 2 and 1 <= d <= 28:
+            return True
+        else:
+            return False
---
+++ 1909/main.py
@@ -0,0 +1,2 @@
+from function import date_checker
+print(date_checker(28, 2, 2016))
