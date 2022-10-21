3f38cc058c0d93e513fcf4ffb523e5b5c3577dd7
Korabnick <dragonytnik@gmail.com>
2022-10-21 12:28:39 +0200

    Заменил == на in для корректности

--- 1909/function.py
+++ 1909/function.py
@@ -1,12 +1,12 @@
 def date_checker(d, m, y):
     months_31 = (1, 3, 5, 7, 8, 10, 12)
     months_30 = (4, 6, 9, 11)
-    if m == months_31:
+    if m in months_31:
         if 1 <= d <= 31:
             return True
         else:
             return False
-    elif m == months_30:
+    elif m in months_30:
         if 1 <= d <= 30:
             return True
         else:
